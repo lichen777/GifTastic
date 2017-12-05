ab8b9e9376f08c5e04fd61382090ea6df66d332c
lichen777 <lchen139@gmail.com>
2017-12-05 12:02:19 -0600

    fix javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,60 @@
 const app = {
   tags : ["dogs", "cats", "birds", "goats", "lions", "pigs"],
   q : "animals",
-  limit : 12,
-  address : "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=" + limit + "&q=",
+  address : "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=" + 12 + "&q=",
 
   renderButtons : function () {
-    
-  }
+  	var input = $("#tagToAdd").val().trim();
+
+  	if (input && this.tags.indexOf(input) === -1) {
+  	  this.tags.push(input);
+	  console.log(this.tags);
+	  this.q = input;
+	  this.searchGif();
+  	}
+
+  	$("#tagToAdd").val("");
 
 
-}
+  	$('#tags').empty();
 
-function renderButtons () {
-  $("#tags").empty();
-  for (var i = 0; i < tags.length; i++) {
-    var button = $('<button class="mx-1 btn btn-secondary btn-sm">').attr("data-animal",tags[i]).text(tags[i]);
-    $("#tags").append(button);
-  }
+  	for (var i = 0; i < this.tags.length; i++) {
+      var button = $('<button>').attr("data-animal",this.tags[i]).addClass('mx-1 btn btn-secondary btn-sm').text(this.tags[i]);
+      $("#tags").append(button);
+    }
 
-};
+  	$(".btn-sm").click(function() {
+      console.log(this);
+	  app.q = $(this).attr("data-animal");
+	  app.searchGif();
+    });
+  },
 
-function showImg () {
-  $("#display").empty();
+  searchGif : function () {
 
-  var queryURL = address + q;
-  console.log(queryURL);
+  	var queryURL = this.address + this.q;
+  	console.log(queryURL);
 
-  $.ajax({
+  	$.ajax({
     url: queryURL,
     method: "GET"
-  })
-  .done(function(response) {
-    var results = response.data;  
-	//console.log(results)
+    })
+    .done(function(response) {
+	  app.showImg(response);
+	});
+  },
+
+  showImg : function (response) {
+  	var results = response.data;
+
+  	$("#display").empty();
 
   	for (var i = 0; i < results.length; i++) {
   	  var gifDiv = $('<div class="col-xs-18 col-sm-6 col-md-3">');
-  	  var rating = results[i].rating;
-  	  var p = $("<div>").text("Rating: " + rating);
-  	  var thumbnail = $('<div>')
-        var animalImg = $("<img>");
+	  var rating = results[i].rating;
+	  var p = $('<div class="caption">').text("Rating: " + rating);
+	  var thumbnail = $('<div class="thumbnail">')
+      var animalImg = $("<img>");
   	  animalImg.attr("src", results[i].images.fixed_height_still.url).attr("data-still", results[i].images.fixed_height_still.url)
   	  .attr("data-animate", results[i].images.fixed_height.url).attr("data-state", "still").addClass("gif");
   	  
@@ -48,47 +63,33 @@ function showImg () {
       gifDiv.append(thumbnail);
 
   	  $("#display").append(gifDiv);
-	  }
-  });
+  	}
+
+  	$(".gif").click(function () {
+      var state = $(this).attr("data-state");
+      //console.log(this);
+
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+      	$(this).attr("data-state", "animate");
+      } else { 
+      	$(this).attr("src", $(this).attr("data-still"));
+      	$(this).attr("data-state", "still");
+      }
+  	});
+
+  }
+
 }
 
 $(document).ready(function() {
 
-	renderButtons();
-  showImg();
+  app.renderButtons();
+  app.searchGif();
 
-
-	$(".btn-sm").click(function() {
-	  console.log(this);
-	  q = $(this).attr("data-animal");
-	  showImg();
-
-	});
-
-	$("#submit").click(function (event) {
-	  event.preventDefault();
-	  var input = $("#tagToAdd").val().trim();
-	  tags.push(input);
-	  renderButtons();
-	  $("#tagToAdd").val("");
-	});
-
-	$(".gif").click(function () {
-    var state = $(this).attr("data-state");
-    console.log(this);
-
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else { 
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-      }
+  $("#submit").click(function (event) {
+    event.preventDefault();	
+	app.renderButtons();
   });
 
 })
-
-
-
-
-
